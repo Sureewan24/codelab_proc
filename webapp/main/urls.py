abc5bf22a7dcb01cd2dbda074d8101a70aae87ec
Sureewan24 <sureewano63@nu.ac.th>
2022-10-03 13:43:48 +0700

    first commit

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -1,13 +1,13 @@
+
+
 #urls.py
 
 from django.urls import path
-from .views import (
-    list_view, 
-    #Work exercise ,Insert code here    
-)
+from .views import *
 
 app_name = 'main'
 urlpatterns = [
     path('', list_view, name='home-list'),
-    #/Work exercise ,Insert code here
+    path('join-now/', join),
+
 ]
